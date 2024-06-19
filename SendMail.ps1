b0a1339a5eac8ee2dcf7eb9f9930841edce923c1
Luka Thomas <102391724+LukkyTech1@users.noreply.github.com>
2024-06-19 14:18:59 +0200

    Update SendMail.ps1

--- SendMail.ps1
+++ SendMail.ps1
@@ -1,11 +1,11 @@
 <#PSScriptInfo
 .VERSION 1.0
-.AUTHOR Niels
+.AUTHOR Luka Meester
 #>
  
 #Password
 $username = "powershell2@sunnus.nl"
-$topsecret = "8lRiHzXc" #never store passwords like this.
+$topsecret = "8lRiHzXc" #
 $password = ConvertTo-SecureString $topsecret -AsPlainText -Force
 $psCred = New-Object System.Management.Automation.PSCredential -ArgumentList ($username, $password)
  
@@ -14,12 +14,12 @@ $smtp = "smtp.sunnus.nl"
 $port = 587
  
 #Address
-$to = "175723@student.horizoncollege.nl" # aanpassen!
+$to = "177972@student.horizoncollege.nl" # aanpassen!
 $from = "powershell2@sunnus.nl"
  
 #Message
 $subject = "Storing op server: Test"
-$html = Get-Content -path "C:\Users\niels\sendmail opdracht\mail.html"
+$html = Get-Content -path "C:\Users\likok\Downloads\Template.html"
  
 #Send MailMessage
 Send-MailMessage -Body ($html | Out-String) -BodyAsHtml -To $to -Subject $subject -SmtpServer $smtp -From $from -Credential $psCred -Port $port
